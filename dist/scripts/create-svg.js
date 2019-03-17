73229a133606ab46a86278357b0f9335ed3443ae
Moltus <moltus@protonmail.com>
2019-03-17 17:31:48 +0100

    working in firefox not other browsers

--- dist/scripts/create-svg.js
+++ dist/scripts/create-svg.js
@@ -1,50 +1,74 @@
 
-
 let width = window.innerWidth;
 let height = window.innerHeight;
-let circleRadius = width / 30;
-let circleTotalH = 11
-let circleTotalV = Math.ceil(height / (width / 10)) + 1 ; 
-let circlesIds = [];
+let inkDropRadius = width / 50;
+let inkDropTotal = 30;
+let inkDropsIds = [];
+const inkDropColors = ['#6cce74', '#c18d46', '#c14745', '#2e1e5b', '#9c37a6', '#76bdd1'];
 
 let container = document.getElementById("img-container");
 
 // create svg and all subelements using .createElementNS
 const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttributeNS(null, "viewBow", `0 0 ${width} ${height}`);
-svg.setAttributeNS(null, "class", "screen-mask");
+svg.setAttributeNS(null, "class", "ink-drop");
 svg.setAttributeNS(null, "width", width);
 svg.setAttributeNS(null, "height", height);
 container.appendChild(svg);
-const defs = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
-svg.appendChild(defs);
-const clipPath = document.createElementNS("http://www.w3.org/2000/svg", 'clipPath');
-clipPath.id = "circlesClipPath";
-defs.appendChild(clipPath);
-for(let x = 0; x < circleTotalH; x++) {
-  for(let y = 0; y < circleTotalV; y++) {
-    let circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-    circle.setAttributeNS(null, "class", "circle");
-    circle.id = 'x' + x + " y" + y;
-    circlesIds.push(circle.id);
-    circle.setAttributeNS(null, "cx", x * width / 10 + y % 2* width / 20);
-    circle.setAttributeNS(null, "cy", y * width / 10);
-    circle.setAttributeNS(null, "r", circleRadius);
-    clipPath.appendChild(circle);
-  }
+const FiltersGroup = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
+svg.appendChild(FiltersGroup);
+const InksGroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+InksGroup.id = "inks-group";
+svg.appendChild(InksGroup);
+
+function getInkInfo() {
+  let posX = Math.floor(Math.random() * (width - 100)) + 50;
+  let posY = Math.floor(Math.random() * (height - 100)) + 50;
+  let color = inkDropColors[Math.floor(Math.random() * inkDropColors.length)];
+  return [posX, posY, color];
 }
 
-let interval = setInterval(showRndCircle, 50);
+for(let i=0; i < inkDropTotal; i++) {
+  // create inkDrop svg circles and define their default scale : 0;
+  [posX, posY, color] = getInkInfo();
+  let inkDrop = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+  inkDrop.id = 'x' + posX + "-y" + posY;
+  inkDropsIds.push(inkDrop.id);
+  inkDrop.setAttributeNS(null, "cx", posX);
+  inkDrop.setAttributeNS(null, "cy", posY);
+  inkDrop.setAttributeNS(null, "r", inkDropRadius);
+
+  inkDrop.style.fill = color;
+  inkDrop.style.transform = 'scale(0)';
+  inkDrop.style.transformOrigin = posX +  "px " + posY + "px";
+  inkDrop.style.filter = 'blur(5px)';
+
+  
+  inkDrop.setAttributeNS(null, "class", "ink-drop");
+
+  // create blur filers for circles and set their default deviation : 0;
+  // let filter = document.createElementNS("http://www.w3.org/2000/svg", "filter");
+  // filter.id = inkDrop.id + "--filter";
+  // let blur = document.createElementNS("http://www.w3.org/2000/svg", "feGaussianBlur");
+  // blur.setAttributeNS(null, "in", "SourceGraphic");
+  // blur.setAttributeNS(null, "stdDeviation", "20");
+  // filter.appendChild(blur);
+  // FiltersGroup.appendChild(filter);
+  InksGroup.appendChild(inkDrop);
+}
+
+let interval = setInterval(showRndCircle, 100);
 
 function showRndCircle() {
-  if (circlesIds.length === 0) {
+  if (inkDropsIds.length === 0) {
     clearInterval(interval);
   } else {
-    let rndIndex = Math.floor((Math.random() * circlesIds.length));
-    let rndId =  circlesIds[rndIndex];
-    circlesIds.splice(rndIndex, 1)
-    document.getElementById(rndId).style.transform = 'scale(1)';
-    document.getElementById(rndId).style.opacity = 1;
+    let rndIndex = Math.floor(Math.random() * inkDropsIds.length);
+    let rndId =  inkDropsIds[rndIndex];
+    inkDropsIds.splice(rndIndex, 1)
+    document.getElementById(rndId).style.transform = 'scale(10)';
+    document.getElementById(rndId).style.filter = 'blur(20px)';
+    // document.getElementById(rndId).style.opacity = 1;
     
   }
 }
