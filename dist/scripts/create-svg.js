dc7576c621a01cd742ed6b82e33867a0c213a7f3
Moltus <moltus@protonmail.com>
2019-03-17 03:22:59 +0100

    added some random fade in with on clippath circles

--- dist/scripts/create-svg.js
+++ dist/scripts/create-svg.js
@@ -1,8 +1,11 @@
+
+
 let width = window.innerWidth;
 let height = window.innerHeight;
 let circleRadius = width / 30;
 let circleTotalH = 11
 let circleTotalV = Math.ceil(height / (width / 10)) + 1 ; 
+let circlesIds = [];
 
 let container = document.getElementById("img-container");
 
@@ -23,9 +26,25 @@ for(let x = 0; x < circleTotalH; x++) {
     let circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     circle.setAttributeNS(null, "class", "circle");
     circle.id = 'x' + x + " y" + y;
+    circlesIds.push(circle.id);
     circle.setAttributeNS(null, "cx", x * width / 10 + y % 2* width / 20);
     circle.setAttributeNS(null, "cy", y * width / 10);
     circle.setAttributeNS(null, "r", circleRadius);
     clipPath.appendChild(circle);
+  }
+}
+
+let interval = setInterval(showRndCircle, 50);
+
+function showRndCircle() {
+  if (circlesIds.length === 0) {
+    clearInterval(interval);
+  } else {
+    let rndIndex = Math.floor((Math.random() * circlesIds.length));
+    let rndId =  circlesIds[rndIndex];
+    circlesIds.splice(rndIndex, 1)
+    document.getElementById(rndId).style.transform = 'scale(1)';
+    document.getElementById(rndId).style.opacity = 1;
+    
   }
 }
