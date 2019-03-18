23635276223abe5a433db481130969eac477b25c
Moltus <moltus@protonmail.com>
2019-03-18 03:40:55 +0100

    working blurred colors...

--- dist/scripts/create-svg.js
+++ dist/scripts/create-svg.js
@@ -1,8 +1,8 @@
 
 let width = window.innerWidth;
 let height = window.innerHeight;
-let inkDropRadius = width / 50;
-let inkDropTotal = 30;
+let inkDropRadius = (width > height) ? width / 50 : height / 50;
+let inkDropTotal = 50;
 let inkDropsIds = [];
 const inkDropColors = ['#6cce74', '#c18d46', '#c14745', '#2e1e5b', '#9c37a6', '#76bdd1'];
 
@@ -10,20 +10,21 @@ let container = document.getElementById("img-container");
 
 // create svg and all subelements using .createElementNS
 const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttributeNS(null, "viewBow", `0 0 ${width} ${height}`);
-svg.setAttributeNS(null, "class", "ink-drop");
-svg.setAttributeNS(null, "width", width);
-svg.setAttributeNS(null, "height", height);
+svg.setAttributeNS(null, "viewBow", `-100 -100 ${width+100} ${height+100}`);
+// svg.setAttributeNS(null, "class", "ink-drop");
+svg.id = "inks";
+svg.setAttributeNS(null, "width", width+200);
+svg.setAttributeNS(null, "height", height+200);
 container.appendChild(svg);
-const FiltersGroup = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
-svg.appendChild(FiltersGroup);
+// const FiltersGroup = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
+// svg.appendChild(FiltersGroup);
 const InksGroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
 InksGroup.id = "inks-group";
 svg.appendChild(InksGroup);
 
 function getInkInfo() {
-  let posX = Math.floor(Math.random() * (width - 100)) + 50;
-  let posY = Math.floor(Math.random() * (height - 100)) + 50;
+  let posX = Math.floor(Math.random() * (width)) + 1;
+  let posY = Math.floor(Math.random() * (height)) + 1;
   let color = inkDropColors[Math.floor(Math.random() * inkDropColors.length)];
   return [posX, posY, color];
 }
@@ -41,7 +42,7 @@ for(let i=0; i < inkDropTotal; i++) {
   inkDrop.style.fill = color;
   inkDrop.style.transform = 'scale(0)';
   inkDrop.style.transformOrigin = posX +  "px " + posY + "px";
-  inkDrop.style.filter = 'blur(5px)';
+  // inkDrop.style.filter = 'blur(5px)';
 
   
   inkDrop.setAttributeNS(null, "class", "ink-drop");
@@ -57,7 +58,7 @@ for(let i=0; i < inkDropTotal; i++) {
   InksGroup.appendChild(inkDrop);
 }
 
-let interval = setInterval(showRndCircle, 100);
+let interval = setInterval(showRndCircle, 50);
 
 function showRndCircle() {
   if (inkDropsIds.length === 0) {
@@ -65,9 +66,14 @@ function showRndCircle() {
   } else {
     let rndIndex = Math.floor(Math.random() * inkDropsIds.length);
     let rndId =  inkDropsIds[rndIndex];
-    inkDropsIds.splice(rndIndex, 1)
+    inkDropsIds.splice(rndIndex, 1);
+    
+    // document.getElementById(rndId).classList.add("ind-drop--transform");
+    
     document.getElementById(rndId).style.transform = 'scale(10)';
-    document.getElementById(rndId).style.filter = 'blur(20px)';
+    // document.getElementById(rndId).style.filter = 'blur(100px)';
+    document.getElementById("inks").style.filter = 'blur(8vmax)';
+    
     // document.getElementById(rndId).style.opacity = 1;
     
   }
