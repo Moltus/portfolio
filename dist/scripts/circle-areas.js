fe59c31e714f14efec77ab286ed1fdbc89e9692d
Moltus <moltus@protonmail.com>
2019-03-18 09:07:31 +0100

    clip path done on all 3 areas + transition

--- dist/scripts/circle-areas.js
+++ dist/scripts/circle-areas.js
@@ -3,35 +3,64 @@ let strokeWidth = 3;
 let area1Center = (isHorizontal) ? [.25 * width, .5 * height] : [.5 * width, .25 * height];
 let area2Center = (isHorizontal) ? [.5 * width, .5 * height] : [.5 * width, .5 * height];
 let area3Center = (isHorizontal) ? [.75 * width, .5 * height] : [.5 * width, .75 * height];
-let areas = [];
+let circles = [];
 
 function createAreaSVG(center) {
   const areaSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   areaSVG.setAttributeNS(null, "viewBow", `0 0 ${2 * circleRadius + strokeWidth * 2} ${2 * circleRadius + strokeWidth * 2}`);
-  areaSVG.id = "area" + areas.length + 1;
-  areas.push(areaSVG.id);
+  
   areaSVG.setAttributeNS(null, "width", circleRadius * 2 + strokeWidth * 2);
   areaSVG.setAttributeNS(null, "height", circleRadius * 2 + strokeWidth * 2);
 
-  container.appendChild(areaSVG);
-  areaSVG.style.left = center[0] - circleRadius - strokeWidth;
-  areaSVG.style.top = center[1] - circleRadius - strokeWidth;
-
+  // areaSVG.style.left = center[0] - circleRadius - strokeWidth;
+  // areaSVG.style.top = center[1] - circleRadius - strokeWidth;
+  areaSVG.style.transform = `translate(${center[0] - circleRadius - strokeWidth}px, ${center[1] - circleRadius - strokeWidth}px)`;
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
-  circle.id = 'x' + posX + "-y" + posY;
-  // circlesIds.push(circle.id);
-  circle.setAttributeNS(null, "cx", circleRadius + 3);
-  circle.setAttributeNS(null, "cy", circleRadius + 3);
+  circle.id = "circle" + (circles.length + 1);
+  circles.push(circle.id);
+  circle.setAttributeNS(null, "cx", circleRadius + strokeWidth);
+  circle.setAttributeNS(null, "cy", circleRadius + strokeWidth);
   circle.setAttributeNS(null, "r", circleRadius);
 
   circle.style.stroke = "white";
+  circle.style.strokeWidth = strokeWidth;
   circle.style.fill = "transparent";
+
+  const circleBg = document.createElement('div');
+  circleBg.className = "circle-bg";
+  circleBg.id = "circle-bg" + (circles.length)
+  circleBg.style.left = center[0] - circleRadius + "px";
+  circleBg.style.top = center[1] - circleRadius + "px";
+  circleBg.style.width = circleRadius * 2 + "px";
+  circleBg.style.height = circleRadius  * 2 + "px";
+  // circleBg.style.opacity = "0";
+  // circleBg.style.opacity = "1";
+
+  container.appendChild(circleBg);
+  container.appendChild(areaSVG);
 
   areaSVG.appendChild(circle);
 }
 
-const area1 = createAreaSVG(area1Center);
-const area2 = createAreaSVG(area2Center);
-const area3 = createAreaSVG(area3Center);
+const bg = document.createElement('div');
+bg.className = "bg";
+
+setTimeout(function() {
+  const area1 = createAreaSVG(area1Center)
+  // document.getElementById('circle-bg1').style.opacity = "1";
+}, 1000);
+setTimeout(function() {
+  const area2 = createAreaSVG(area2Center)
+  document.getElementById('circle-bg1').style.opacity = "1";
+}, 2000);
+setTimeout(function() {
+  const area3 = createAreaSVG(area3Center)
+  document.getElementById('circle-bg2').style.opacity = "1";
+}, 3000);
+setTimeout(function() {
+  document.getElementById('circle-bg3').style.opacity = "1";
+}, 4000);
+// const area2 = createAreaSVG(area2Center);
+// const area3 = createAreaSVG(area3Center);
 
