60a3a71637a8787df1f23fd3bfa9e50b1593f778
Moltus <moltus@protonmail.com>
2019-04-20 00:12:46 +0200

    non substantial

--- dist/scripts/disk-rotation.js
+++ dist/scripts/disk-rotation.js
@@ -7,10 +7,10 @@ if (isHorizontal) {
   scrollLeft.addEventListener('click', function() {
     diskList[diskIndex + 3].style.display = "block";
     diskList[diskIndex].style.display = "none";
-    diskIndex -= 1;
+    diskIndex += 1;
     for (let i of diskList) {
       // i.style.transform = `translateX(${-width * .25} px)`;
-      i.style.left = diskIndex * width * .25 + "px";
+      i.style.left = -diskIndex * width * .25 + "px";
     }
   })
 
@@ -18,7 +18,7 @@ if (isHorizontal) {
   scrollRight.addEventListener('click', function() {
     diskList[diskIndex + 3].style.display = "none";
     diskList[diskIndex].style.display = "block";
-    diskIndex += 1;
+    diskIndex -= 1;
     for (let i of diskList) {
       i.style.left = diskIndex * width * .25 + "px";
     }
