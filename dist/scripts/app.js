860c9095d3438cd514ee2c8d6bfd47f9f299dfac
Moltus <moltus@protonmail.com>
2019-04-23 21:18:23 +0200

    commented out filter blur on .ink-drop, added document reload on window resize event

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -2,3 +2,19 @@
 let width = window.innerWidth;
 let height = window.innerHeight;
 let isHorizontal = width > height;
+
+(function () {
+
+  var timeout = null,
+      delay = 250; // How long to wait (in ms) before deciding resize is complete
+
+   window.onresize = function () {
+      // Clear previous timeout to indicate resizing is still occurring
+      window.clearTimeout(timeout);
+
+      timeout = window.setTimeout(function () {
+          // Resizing is (probably) done, now we can reload the document
+          location.reload();
+      }, delay);
+  };
+})();
