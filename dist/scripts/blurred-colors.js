86837b9e6a118d4725a0b21aea3d0a1f90e085fb
Moltus <moltus@protonmail.com>
2020-11-19 03:48:28 +0100

    replaced main content discs with hexagon shape. outline have been temp. removed. to fix : bugged sliding

--- dist/scripts/blurred-colors.js
+++ dist/scripts/blurred-colors.js
@@ -1,64 +1,75 @@
-
-let inkDropRadius = (isHorizontal) ? width / 50 : height / 50;
+let inkDropRadius = isHorizontal ? width / 50 : height / 50;
 let inkDropTotal = 50;
 let inkDropsIds = [];
-const inkDropColors = ['#6cce74', '#c18d46', '#c14745', '#2e1e5b', '#9c37a6', '#76bdd1'];
+const inkDropColors = [
+	'#6cce74',
+	'#c18d46',
+	'#c14745',
+	'#2e1e5b',
+	'#9c37a6',
+	'#76bdd1',
+];
 
-const backgroundContainer = document.getElementById("background-container");
+const backgroundContainer = document.getElementById('background-container');
 
 // create svg and all subelements using .createElementNS
-const inksSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-inksSVG.setAttributeNS(null, "viewBow", `0 0 ${width} ${height}`);
+const inksSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+inksSVG.setAttributeNS(null, 'viewBow', `0 0 ${width} ${height}`);
 // inksSVG.setAttributeNS(null, "class", "ink-drop");
-inksSVG.id = "inks";
-inksSVG.setAttributeNS(null, "width", width);
-inksSVG.setAttributeNS(null, "height", height);
+inksSVG.id = 'inks';
+inksSVG.setAttributeNS(null, 'width', width);
+inksSVG.setAttributeNS(null, 'height', height);
 backgroundContainer.appendChild(inksSVG);
 // const FiltersGroup = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
 // svg.appendChild(FiltersGroup);
-const inksGroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
-inksGroup.id = "inks-group";
+const inksGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+inksGroup.id = 'inks-group';
 inksSVG.appendChild(inksGroup);
 
 function getInkInfo() {
-  let posX = Math.floor(Math.random() * (width)) + 1;
-  let posY = Math.floor(Math.random() * (height)) + 1;
-  let color = inkDropColors[Math.floor(Math.random() * inkDropColors.length)];
-  return [posX, posY, color];
+	let posX = Math.floor(Math.random() * width) + 1;
+	let posY = Math.floor(Math.random() * height) + 1;
+	let color = inkDropColors[Math.floor(Math.random() * inkDropColors.length)];
+	return [posX, posY, color];
 }
 
-for(let i=0; i < inkDropTotal; i++) {
-  // create inkDrop svg circles and define their default scale : 0;
-  [posX, posY, color] = getInkInfo();
-  let inkDrop = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-  inkDrop.id = 'x' + posX + "-y" + posY;
-  inkDropsIds.push(inkDrop.id);
-  inkDrop.setAttributeNS(null, "cx", posX);
-  inkDrop.setAttributeNS(null, "cy", posY);
-  inkDrop.setAttributeNS(null, "r", inkDropRadius);
+for (let i = 0; i < inkDropTotal; i++) {
+	// create inkDrop svg circles and define their default scale : 0;
+	[posX, posY, color] = getInkInfo();
+	let inkDrop = document.createElementNS(
+		'http://www.w3.org/2000/svg',
+		'circle'
+	);
+	inkDrop.id = 'x' + posX + '-y' + posY;
+	inkDropsIds.push(inkDrop.id);
+	inkDrop.setAttributeNS(null, 'cx', posX);
+	inkDrop.setAttributeNS(null, 'cy', posY);
+	inkDrop.setAttributeNS(null, 'r', inkDropRadius);
 
-  inkDrop.style.fill = color;
-  inkDrop.style.transform = 'scale(0)';
-  inkDrop.style.transformOrigin = posX +  "px " + posY + "px";
+	inkDrop.style.fill = color;
+	inkDrop.style.transform = 'scale(0)';
+	inkDrop.style.transformOrigin = posX + 'px ' + posY + 'px';
 
-  inkDrop.setAttributeNS(null, "class", "ink-drop");
+	inkDrop.setAttributeNS(null, 'class', 'ink-drop');
 
-
-  inksGroup.appendChild(inkDrop);
+	inksGroup.appendChild(inkDrop);
 }
 
 let interval = setInterval(showRndCircle, 30);
 
 function showRndCircle() {
-  if (inkDropsIds.length === 0) {
-    clearInterval(interval);
-  } else {
-    let rndIndex = Math.floor(Math.random() * inkDropsIds.length);
-    let rndId =  inkDropsIds[rndIndex];
-    inkDropsIds.splice(rndIndex, 1);
-    
-    document.getElementById(rndId).style.transform = 'scale(10)';
-    document.getElementById("inks").style.filter = 'blur(8vmax)';
-    
-  }
+	if (inkDropsIds.length === 0) {
+		clearInterval(interval);
+	} else {
+		let rndIndex = Math.floor(Math.random() * inkDropsIds.length);
+		let rndId = inkDropsIds[rndIndex];
+		inkDropsIds.splice(rndIndex, 1);
+
+		document.getElementById(rndId).style.transform = 'scale(10)';
+		document.getElementById('inks').style.filter = 'blur(8vmax)';
+	}
 }
+
+// const gridOverlay = document.createElement('div');
+// gridOverlay.id = 'grid-overlay';
+// backgroundContainer.appendChild(gridOverlay);
--- dist/scripts/content-areas.js
+++ dist/scripts/content-areas.js
@@ -1,187 +1,287 @@
 // TODO : tweak values for responsive... bigger disks for small screens
 let disksOnScreen = 3;
-let diskDiameter = (isHorizontal) ? .6 * width / disksOnScreen : .6 * height / disksOnScreen;
+let aeraWidth = isHorizontal
+	? (0.6 * width) / disksOnScreen
+	: (0.6 * height) / disksOnScreen;
+let aeraHeight = aeraWidth * 1.1547005;
 
 let disks = [];
 let diskInc = 0;
 
-const disksContainer = document.getElementById("disks-container");
+const disksContainer = document.getElementById('disks-container');
 
 function createArea(title, url, image, text, opacity, display) {
-  const circularArea = document.createElement("div");
-  let areaCenter = (isHorizontal) ? [diskInc * .25 * width, .5 * height] : [.5 * width, diskInc * .25 * height]
-  circularArea.id = "circular-area" + diskInc;
-  circularArea.className = "circular-area";
-  circularArea.style.position = "absolute";
-  circularArea.style.left = areaCenter[0] - diskDiameter / 2 + "px";
-  circularArea.style.top = areaCenter[1] - diskDiameter / 2 - 20 + "px";
-  circularArea.style.opacity = (opacity) ? "1" : "0";
-  circularArea.style.display = (display) ? "block" : "none";
-  const diskLink = document.createElement("a");
-  diskLink.href = url;
-  diskLink.target = "_blank";
-  diskLink.id = "circular-area__link" + diskInc;
-  diskLink.className = "circular-area__link";
+	const circularArea = document.createElement('div');
+	let areaCenter = isHorizontal
+		? [diskInc * 0.25 * width, 0.5 * height]
+		: [0.5 * width, diskInc * 0.25 * height];
+	circularArea.id = 'circular-area' + diskInc;
+	circularArea.className = 'circular-area';
+	circularArea.style.position = 'absolute';
+	circularArea.style.left = areaCenter[0] - aeraWidth / 2 + 'px';
+	circularArea.style.top = areaCenter[1] - aeraHeight / 2 - 20 + 'px';
+	circularArea.style.opacity = opacity ? '1' : '0';
+	circularArea.style.display = display ? 'block' : 'none';
+	const diskLink = document.createElement('a');
+	diskLink.href = url;
+	diskLink.target = '_blank';
+	diskLink.id = 'circular-area__link' + diskInc;
+	diskLink.className = 'circular-area__link';
 
-  const diskArrow = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  diskArrow.setAttributeNS(null, "class", "disk__arrow");
-  const arrow = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-  diskArrow.appendChild(arrow);
-  diskArrow.style.width = '40px';
-  diskArrow.style.height = '20px';
-  arrow.id = "arrow-icon" + diskInc;
-  arrow.setAttributeNS(null, "class", "disk__arrow__icon");
-  arrow.setAttributeNS(null, "points", "0.5 0.82 0.5 18.74 17.81 9.87 0.5 0.82");
+	const diskArrow = document.createElementNS(
+		'http://www.w3.org/2000/svg',
+		'svg'
+	);
+	diskArrow.setAttributeNS(null, 'class', 'disk__arrow');
+	const arrow = document.createElementNS(
+		'http://www.w3.org/2000/svg',
+		'polygon'
+	);
+	diskArrow.appendChild(arrow);
+	diskArrow.style.width = '40px';
+	diskArrow.style.height = '20px';
+	arrow.id = 'arrow-icon' + diskInc;
+	arrow.setAttributeNS(null, 'class', 'disk__arrow__icon');
+	arrow.setAttributeNS(
+		null,
+		'points',
+		'0.5 0.82 0.5 18.74 17.81 9.87 0.5 0.82'
+	);
 
-  const disk = document.createElement('div');
-  disk.className = "disk";
-  disk.id = "disk" + diskInc;
-  disk.style.width = diskDiameter + "px";
-  disk.style.height = diskDiameter + "px";
+	const disk = document.createElement('div');
+	disk.className = 'disk';
+	disk.id = 'disk' + diskInc;
+	disk.style.width = aeraWidth + 'px';
+	disk.style.height = aeraHeight + 'px';
 
-  const diskTitle = document.createElement("h2");
-  diskTitle.id = "disk__title" + diskInc;
-  diskTitle.className = "disk__title";
-  diskTitle.style.fontSize = diskDiameter / 10 + 'px';
-  const titleNode = document.createTextNode(title);
-  diskTitle.appendChild(titleNode);
+	const diskTitle = document.createElement('h2');
+	diskTitle.id = 'disk__title' + diskInc;
+	diskTitle.className = 'disk__title';
+	diskTitle.style.fontSize = aeraWidth / 11 + 'px';
+	const titleNode = document.createTextNode(title);
+	diskTitle.appendChild(titleNode);
 
-  const diskImage = document.createElement("img");
-  diskImage.id = "disk__image" + diskInc;
-  diskImage.className = "disk__image";
-  diskImage.src = image;
+	const diskImage = document.createElement('img');
+	diskImage.id = 'disk__image' + diskInc;
+	diskImage.className = 'disk__image';
+	diskImage.src = image;
 
-  const diskText = document.createElement("p");
-  diskText.id = "disk__text" + diskInc;
-  diskText.className = "disk__text";
-  // diskText.style.fontSize = diskDiameter / 30 + 'px';
-  const textNode = document.createTextNode(text);
-  diskText.appendChild(textNode);
+	const diskText = document.createElement('p');
+	diskText.id = 'disk__text' + diskInc;
+	diskText.className = 'disk__text';
+	// diskText.style.fontSize = diskDiameter / 30 + 'px';
+	const textNode = document.createTextNode(text);
+	diskText.appendChild(textNode);
 
+	disksContainer.appendChild(circularArea);
 
-  disksContainer.appendChild(circularArea);
-  
-  // diskLink.appendChild(diskArrow);
-  // diskLink.appendChild(disk);
-  circularArea.appendChild(diskArrow);
-  disk.appendChild(diskTitle);
-  disk.appendChild(diskImage);
-  disk.appendChild(diskText);
-  diskLink.appendChild(disk);
-  // circularArea.appendChild(disk);
-  circularArea.appendChild(diskLink);
+	// diskLink.appendChild(diskArrow);
+	// diskLink.appendChild(disk);
+	circularArea.appendChild(diskArrow);
+	disk.appendChild(diskTitle);
+	disk.appendChild(diskImage);
+	disk.appendChild(diskText);
+	diskLink.appendChild(disk);
+	// circularArea.appendChild(disk);
+	circularArea.appendChild(diskLink);
 
-  diskInc ++;
+	diskInc++;
 
-  return circularArea;
+	return circularArea;
 }
 
 function createScrollArrows() {
-  if (isHorizontal) {
+	if (isHorizontal) {
+		// when screen is horizontal create icon for scrolling left
+		const scrollArrowLeft = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'svg'
+		);
+		scrollArrowLeft.setAttributeNS(null, 'class', 'scroll-arrow');
+		scrollArrowLeft.setAttributeNS(null, 'viewbox', '0 0 100 100');
+		scrollArrowLeft.setAttributeNS(null, 'height', '100px');
+		scrollArrowLeft.setAttributeNS(null, 'width', '100px');
+		scrollArrowLeft.id = 'scroll-arrow-left';
+		const arrowLeft = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'polygon'
+		);
+		arrowLeft.setAttributeNS(
+			null,
+			'points',
+			'68,19.1 29.7,49.1 68,79.1 69.2,77.6 32.9,49.1 69.2,20.6 68,19.1 '
+		);
 
-    // when screen is horizontal create icon for scrolling left
-    const scrollArrowLeft = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    scrollArrowLeft.setAttributeNS(null, "class", "scroll-arrow");
-    scrollArrowLeft.setAttributeNS(null, "viewbox", "0 0 100 100");
-    scrollArrowLeft.setAttributeNS(null, "height", "100px");
-    scrollArrowLeft.setAttributeNS(null, "width", "100px");
-    scrollArrowLeft.id = "scroll-arrow-left";
-    const arrowLeft = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    arrowLeft.setAttributeNS(null, "points", "68,19.1 29.7,49.1 68,79.1 69.2,77.6 32.9,49.1 69.2,20.6 68,19.1 ");
+		scrollArrowLeft.style.left = 0.04 * width - 20 + 'px';
+		scrollArrowLeft.style.top = 0.5 * height - 40 + 'px';
+		scrollArrowLeft.appendChild(arrowLeft);
+		disksContainer.appendChild(scrollArrowLeft);
+		scrollArrowLeft.style.transform = `scale(${width / 1000})`;
+		scrollArrowLeft.style.display = 'none';
 
-    scrollArrowLeft.style.left = .04 * width - 20 + "px";
-    scrollArrowLeft.style.top = .5 * height - 40 + "px";
-    scrollArrowLeft.appendChild(arrowLeft);
-    disksContainer.appendChild(scrollArrowLeft);
-    scrollArrowLeft.style.transform = `scale(${width/1000})`;
-    scrollArrowLeft.style.display = "none";
+		// when screen is horizontal create icon for scrolling right
+		const scrollArrowRight = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'svg'
+		);
+		scrollArrowRight.setAttributeNS(null, 'class', 'scroll-arrow');
+		scrollArrowRight.setAttributeNS(null, 'viewbox', '0 0 100 100');
+		scrollArrowRight.setAttributeNS(null, 'height', '100px');
+		scrollArrowRight.setAttributeNS(null, 'width', '100px');
+		scrollArrowRight.id = 'scroll-arrow-right';
+		const arrowRight = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'polygon'
+		);
+		arrowRight.setAttributeNS(
+			null,
+			'points',
+			'30.4,20.6 66.7,49.1 30.4,77.6 31.6,79.1 69.9,49.1 31.6,19.1 30.4,20.6 '
+		);
 
+		scrollArrowRight.style.right = 0.04 * width - 20 + 'px';
+		scrollArrowRight.style.top = 0.5 * height - 40 + 'px';
+		scrollArrowRight.appendChild(arrowRight);
+		disksContainer.appendChild(scrollArrowRight);
+		scrollArrowRight.style.transform = `scale(${width / 1000})`;
+		scrollArrowRight.style.display = 'none';
 
-    // when screen is horizontal create icon for scrolling right
-    const scrollArrowRight = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    scrollArrowRight.setAttributeNS(null, "class", "scroll-arrow");
-    scrollArrowRight.setAttributeNS(null, "viewbox", "0 0 100 100");
-    scrollArrowRight.setAttributeNS(null, "height", "100px");
-    scrollArrowRight.setAttributeNS(null, "width", "100px");
-    scrollArrowRight.id = "scroll-arrow-right";
-    const arrowRight = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    arrowRight.setAttributeNS(null, "points", "30.4,20.6 66.7,49.1 30.4,77.6 31.6,79.1 69.9,49.1 31.6,19.1 30.4,20.6 ");
+		return [scrollArrowLeft, scrollArrowRight];
+	} else {
+		// when screen is vertical create icon for scrolling top
+		const scrollArrowTop = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'svg'
+		);
+		scrollArrowTop.setAttributeNS(null, 'class', 'scroll-arrow');
+		scrollArrowTop.setAttributeNS(null, 'viewbox', '0 0 100 100');
+		scrollArrowTop.setAttributeNS(null, 'height', '100px');
+		scrollArrowTop.setAttributeNS(null, 'width', '100px');
+		scrollArrowTop.id = 'scroll-arrow-top';
+		const arrowTop = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'polygon'
+		);
+		arrowTop.setAttributeNS(
+			null,
+			'points',
+			'22.4,64.1 50.8,36.5 79.3,64.1 80.8,63.2 50.8,34.1 20.8,63.2 22.4,64.1 '
+		);
 
-    scrollArrowRight.style.right = .04 * width - 20 + "px";
-    scrollArrowRight.style.top = .5 * height - 40 + "px";
-    scrollArrowRight.appendChild(arrowRight);
-    disksContainer.appendChild(scrollArrowRight);
-    scrollArrowRight.style.transform = `scale(${width/1000})`;
-    scrollArrowRight.style.display = "none";
+		scrollArrowTop.style.left = 0.5 * width - 50 + 'px';
+		scrollArrowTop.style.top = 0.04 * height - 20 + 'px';
+		scrollArrowTop.appendChild(arrowTop);
+		disksContainer.appendChild(scrollArrowTop);
+		scrollArrowTop.style.transform = `scale(${height / 1000})`;
+		scrollArrowTop.style.display = 'none';
 
-    return [scrollArrowLeft, scrollArrowRight];
+		// when screen is vertical create icon for scrolling bottom
+		const scrollArrowBottom = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'svg'
+		);
+		scrollArrowBottom.setAttributeNS(null, 'class', 'scroll-arrow');
+		scrollArrowBottom.setAttributeNS(null, 'viewbox', '0 0 100 100');
+		scrollArrowBottom.setAttributeNS(null, 'height', '100px');
+		scrollArrowBottom.setAttributeNS(null, 'width', '100px');
+		scrollArrowBottom.id = 'scroll-arrow-bottom';
+		const arrowBottom = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'polygon'
+		);
+		arrowBottom.setAttributeNS(
+			null,
+			'points',
+			'23.1,34.1 51.5,61.7 80,34.1 81.5,35 51.5,64.1 21.5,35 23.1,34.1 '
+		);
 
-  } else {
-    // when screen is vertical create icon for scrolling top
-    const scrollArrowTop = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    scrollArrowTop.setAttributeNS(null, "class", "scroll-arrow");
-    scrollArrowTop.setAttributeNS(null, "viewbox", "0 0 100 100");
-    scrollArrowTop.setAttributeNS(null, "height", "100px");
-    scrollArrowTop.setAttributeNS(null, "width", "100px");
-    scrollArrowTop.id = "scroll-arrow-top";
-    const arrowTop = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    arrowTop.setAttributeNS(null, "points", "22.4,64.1 50.8,36.5 79.3,64.1 80.8,63.2 50.8,34.1 20.8,63.2 22.4,64.1 ");
+		scrollArrowBottom.style.left = 0.5 * width - 50 + 'px';
+		scrollArrowBottom.style.bottom = 0.04 * height - 20 + 'px';
+		scrollArrowBottom.appendChild(arrowBottom);
+		disksContainer.appendChild(scrollArrowBottom);
+		scrollArrowBottom.style.transform = `scale(${height / 1000})`;
+		scrollArrowBottom.style.display = 'none';
 
-    scrollArrowTop.style.left = .5 * width - 50 + "px";
-    scrollArrowTop.style.top = .04 * height - 20 + "px";
-    scrollArrowTop.appendChild(arrowTop);
-    disksContainer.appendChild(scrollArrowTop);
-    scrollArrowTop.style.transform = `scale(${height/1000})`;
-    scrollArrowTop.style.display = "none";
-
-
-    // when screen is vertical create icon for scrolling bottom
-    const scrollArrowBottom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    scrollArrowBottom.setAttributeNS(null, "class", "scroll-arrow");
-    scrollArrowBottom.setAttributeNS(null, "viewbox", "0 0 100 100");
-    scrollArrowBottom.setAttributeNS(null, "height", "100px");
-    scrollArrowBottom.setAttributeNS(null, "width", "100px");
-    scrollArrowBottom.id = "scroll-arrow-bottom";
-    const arrowBottom = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    arrowBottom.setAttributeNS(null, "points", "23.1,34.1 51.5,61.7 80,34.1 81.5,35 51.5,64.1 21.5,35 23.1,34.1 ");
-
-    scrollArrowBottom.style.left = .5 * width - 50 + "px";
-    scrollArrowBottom.style.bottom = .04 * height - 20 + "px";
-    scrollArrowBottom.appendChild(arrowBottom);
-    disksContainer.appendChild(scrollArrowBottom);
-    scrollArrowBottom.style.transform = `scale(${height/1000})`;
-    scrollArrowBottom.style.display = "none";
-
-    return [scrollArrowTop, scrollArrowBottom];
-  }
+		return [scrollArrowTop, scrollArrowBottom];
+	}
 }
 
 const bg = document.createElement('div');
-bg.className = "bg";
+bg.className = 'bg';
 
-disks.push(createArea("AUTRES TRAVAUX", "https://github.com/benoitclement", "images/my-github.png", "Le code de tous mes projets sur ma page github.", opacity=false, display=true));
-disks.push(createArea("HOTEL PARIMIS", "https://parimis.benoitclement.fr", "images/parimis.png", "Site vitrine 100% responsive pour un hôtel de luxe d'après cahier des charges. HTML5, PostCSS, Javascript, Gulp, Git.", opacity=true, display=false));
-disks.push(createArea("MIND-MAPPING", "../cv2019", "images/mind-map.png", "Outil de mise en forme façon carte heuristique de contenus interconnectés de type texte ou image. Le but est à terme de produire une interface complète WYSIWYG pour la mise en relation d'informations." , opacity=true, display=false));
-disks.push(createArea("VEVILLE LOCATIONS", "../veville", "images/veville.png", "Site de location de véhicules en ligne avec backoffice d'après cahier des charges. Bases de données : utilisateurs, véhicules, agences, commandes. HTML5, CSS3, Bootstrap, PHP, MySQL, Ajax, Git.", opacity=true, display=false));
-disks.push(createArea("MATCH-3 BOT", "https://github.com/benoitclement/match3-bot", "images/match3bot.png", "Programme \"bot\" / solutionneur de puzzle game type match-3 (ex. Candy Crush). Analyse en temps réel l'affichage écran avant de décider du meilleur mouvement selon un système de priorités. Réalisé avec Python, OpenCV, Numpy.", opacity=false, display=true));
-disks.push(createArea("GUESS THE WORD", "https://github.com/benoitclement/guess-a-word", "images/guess-the-word.png", "Programme de jeu établissant une liste de 10 mots à deviner avec 3 essais pour chacun. Le joueur forme les mots en glissant-déplaçant les lettres. Python, TKinter.", opacity=false, display=true));
+disks.push(
+	createArea(
+		'AUTRES TRAVAUX',
+		'https://github.com/benoitclement',
+		'images/my-github.png',
+		'Le code de tous mes projets sur ma page github.',
+		(opacity = false),
+		(display = true)
+	)
+);
+disks.push(
+	createArea(
+		'HOTEL PARIMIS',
+		'https://parimis.benoitclement.fr',
+		'images/parimis.png',
+		"Site vitrine 100% responsive pour un hôtel de luxe d'après cahier des charges. HTML5, PostCSS, Javascript, Gulp, Git.",
+		(opacity = true),
+		(display = false)
+	)
+);
+disks.push(
+	createArea(
+		'MIND-MAPPING',
+		'../cv2019',
+		'images/mind-map.png',
+		"Outil de mise en forme façon carte heuristique de contenus interconnectés de type texte ou image. Le but est à terme de produire une interface complète WYSIWYG pour la mise en relation d'informations.",
+		(opacity = true),
+		(display = false)
+	)
+);
+disks.push(
+	createArea(
+		'VEVILLE LOCATIONS',
+		'../veville',
+		'images/veville.png',
+		"Site de location de véhicules en ligne avec backoffice d'après cahier des charges. Bases de données : utilisateurs, véhicules, agences, commandes. HTML5, CSS3, Bootstrap, PHP, MySQL, Ajax, Git.",
+		(opacity = true),
+		(display = false)
+	)
+);
+disks.push(
+	createArea(
+		'MATCH-3 BOT',
+		'https://github.com/benoitclement/match3-bot',
+		'images/match3bot.png',
+		'Programme "bot" / solutionneur de puzzle game type match-3 (ex. Candy Crush). Analyse en temps réel l\'affichage écran avant de décider du meilleur mouvement selon un système de priorités. Réalisé avec Python, OpenCV, Numpy.',
+		(opacity = false),
+		(display = true)
+	)
+);
+disks.push(
+	createArea(
+		'GUESS THE WORD',
+		'https://github.com/benoitclement/guess-a-word',
+		'images/guess-the-word.png',
+		'Programme de jeu établissant une liste de 10 mots à deviner avec 3 essais pour chacun. Le joueur forme les mots en glissant-déplaçant les lettres. Python, TKinter.',
+		(opacity = false),
+		(display = true)
+	)
+);
 
 const scrollArrows = createScrollArrows();
 
-setTimeout(function() {
-  disks[1].style.display = "block";
+setTimeout(function () {
+	disks[1].style.display = 'block';
 }, 500);
-setTimeout(function() {
-  disks[2].style.display = "block";
+setTimeout(function () {
+	disks[2].style.display = 'block';
 }, 1000);
-setTimeout(function() {
-  disks[3].style.display = "block";
+setTimeout(function () {
+	disks[3].style.display = 'block';
 }, 1500);
-setTimeout(function() {
-  scrollArrows[0].style.display = "block";
-  scrollArrows[1].style.display = "block";
-  
+setTimeout(function () {
+	scrollArrows[0].style.display = 'block';
+	scrollArrows[1].style.display = 'block';
 }, 2000);
-
-
-
